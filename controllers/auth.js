6b3dca4e7003a6ae01a591224762c6b48f5c03fb
javiergranadosr <javiergranadosrdev@gmail.com>
2022-06-04 16:41:05 -0500

    Se agrego inicio de sesion con google

--- controllers/auth.js
+++ controllers/auth.js
@@ -2,6 +2,7 @@ const { response } = require("express");
 const bcryptjs = require("bcryptjs");
 const User = require("../models/user");
 const { generateJwt } = require("../helpers/generateJwt");
+const { googleVerify } = require("../helpers/googleVerify");
 
 const login = async (req, res = response) => {
   try {
@@ -40,4 +41,45 @@ const login = async (req, res = response) => {
   }
 };
 
-module.exports = { login };
+const googleSignIn = async (req, res = response) => {
+  try {
+    const { tokenId } = req.body;
+
+    const { name, email, picture } = await googleVerify(tokenId);
+
+    let user = await User.findOne({ email });
+
+    if (!user) {
+      const data = {
+        name,
+        email,
+        password: "123456",
+        image: picture,
+        google: true,
+        rol: "USER_ROLE",
+      };
+
+      user = new User(data);
+      await user.save();
+    }
+
+    if (!user.status) {
+      return res
+        .status(401)
+        .json({ message: "Hable con el administrador. Usuario bloqueado" });
+    }
+
+    // TODO: Generar JWT
+    const token = await generateJwt(user.id);
+
+    res.json({
+      user,
+      token,
+    });
+  } catch (error) {
+    console.log(error);
+    res.status(500).json({ message: "Error hable con un administrador." });
+  }
+};
+
+module.exports = { login, googleSignIn };
---
+++ helpers/googleVerify.js
@@ -0,0 +1,18 @@
+const { OAuth2Client } = require("google-auth-library");
+const client = new OAuth2Client(process.env.GOOGLE_CLIENT_ID);
+
+async function googleVerify(tokenId = "") {
+  const ticket = await client.verifyIdToken({
+    idToken: tokenId,
+    audience: process.env.GOOGLE_CLIENT_ID,
+  });
+  const payload = ticket.getPayload();
+
+  const { name, picture, email } = payload;
+
+  return { name, picture, email };
+}
+
+module.exports = {
+  googleVerify,
+};
